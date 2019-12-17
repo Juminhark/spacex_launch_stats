f137c9ac26dca70e78c33df602efcfafb93d7c8a
Juminhark <40381241+Juminhark@users.noreply.github.com>
2019-12-17 18:04:35 +0900

    Express GraphQL Server

--- schema.js
+++ schema.js
@@ -15,8 +15,8 @@ const LaunchType = new GraphQLObjectType({
     fields: () => ({
         flight_number: { type: GraphQLInt },
         mission_name: { type: GraphQLString },
-        launch_year: { type: GraphQLInt },
-        launch_date_local: { type: GraphQLInt },
+        launch_year: { type: GraphQLString },
+        launch_date_local: { type: GraphQLString },
         launch_success: { type: GraphQLBoolean },
         rocket: { type: RocketType }
     })
@@ -41,6 +41,36 @@ const RootQuery = new GraphQLObjectType({
             type: new GraphQLList(LaunchType),
             resolve(parent, args) {
                 return axios.get('https://api.spacexdata.com/v3/launches')
+                    .then(res => res.data);
+            }
+        },
+        launch: {
+            type: LaunchType,
+            args: {
+                flight_number: { type: GraphQLInt}
+            },
+            resolve(parent,args){
+                return axios
+                    .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+                    .then(res => res.data);
+            }
+        },
+        rockets: {
+            type: new GraphQLList(RocketType),
+            resolve(parent, args) {
+                return axios
+                    .get('https://api.spacexdata.com/v3/rockets')
+                    .then(res => res.data);
+            }
+        },
+        rocket: {
+            type: RocketType,
+            args: {
+                id: { type: GraphQLInt}
+            },
+            resolve(parent,args){
+                return axios
+                    .get(`https://api.spacexdata.com/v3/rocket/${args.id}`)
                     .then(res => res.data);
             }
         }
